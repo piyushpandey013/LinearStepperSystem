a5baac43fbc3d0dfa5df69b0944d859021780b9e
Makerboost <tore.waldow@gmail.com>
2015-05-19 12:22:52 +0200

    second commit

---
+++ LinearStepperSystem/main.c
@@ -0,0 +1,56 @@
+/*
+ * main.c
+ *
+ * Created: 15.05.2015 22:35:11
+ *  Author: Makerboost
+ */ 
+
+
+
+/*
+	INCLUDES
+*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include "main.h"
+
+
+
+/*
+	PORTS
+*/
+
+// TODO: Set step output pin
+// TODO: Set direction output pin
+
+
+
+/*
+	TIMERS
+*/
+
+// TODO: Set up timer for step output. 16 bit timer, to allow for longer pause when ramping up or down.
+//	pgm_read_byte(&delayTable[0]);
+
+
+/*
+	SERIAL SETUP
+	Listen on serial port for commands and then act on them
+	Commands are a subset of G-Code
+*/
+
+
+
+int main(void)
+{
+    while(1)
+    {
+        
+		
+		
+		
+		
+		
+    }
+}
